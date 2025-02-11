85b90e476f00aa2af6637c87a9fac2b1f7b9fcca
Anna Mercado <nissathar.15@gmail.com>
2025-02-11 11:05:16 +0100

    exercici 1

---
+++ Tema 2/mongo.js
@@ -0,0 +1,52 @@
+{"costumer": [
+        "id":,
+        "name":"",
+        "surname":"",
+        "address":"",
+        "phone":"",
+        "email":"",
+        "register_date":"",
+        "recommended_by":{
+            "id":,
+        "address": {
+            "street":"",
+            "number":0,
+            "floor":"",
+            "door":"",
+            "city":"",
+            "zip_code":0,
+            "country":""
+        },
+    ]
+    
+    "Shoppings": [
+      {
+        "id":,
+        "brand":"",
+        "mount":"",
+        "color_mount":"",
+        "graduation":0,
+        "color_l":"",
+        "color_r":"",
+        "price":0,
+        "seller":"",
+        "supplier": 
+          {
+            "id":"",
+            "name":"",
+            "phone":"",
+            "fax":"",
+            "NIF":"",
+            "address": {
+              "street":"",
+              "number":0,
+              "floor":"",
+              "door":"",
+              "city":"",
+              "zip_code":0,
+              "country":""
+            }
+          }
+      }
+    ]
+  }
